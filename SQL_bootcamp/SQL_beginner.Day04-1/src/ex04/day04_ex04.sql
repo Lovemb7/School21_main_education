dc43c525b86b2c0648b26fafe5e80837ee3457ef
finchren <finchren@student.21-school.ru>
2023-06-03 16:50:18 +0300

    Fix the forth exercise

--- SQL_bootcamp/SQL_beginner.Day04-1/src/ex04/day04_ex04.sql
+++ SQL_bootcamp/SQL_beginner.Day04-1/src/ex04/day04_ex04.sql
@@ -1,8 +1,10 @@
 CREATE VIEW v_symmetric_union AS
 WITH
-R AS (SELECT * FROM person_visits AS R WHERE visit_date = '2022-01-02'),
-S AS (SELECT * FROM person_visits AS S WHERE visit_date = '2022-01-06')
-SELECT * FROM R EXCEPT SELECT * FROM S
-UNION ALL
-SELECT * FROM S EXCEPT SELECT * FROM R
+R AS (SELECT person_id FROM person_visits WHERE visit_date = '2022-01-02'),
+S AS (SELECT person_id FROM person_visits WHERE visit_date = '2022-01-06')
+SELECT person_id FROM (
+SELECT * FROM R EXCEPT SELECT * FROM S) as table1
+UNION
+SELECT person_id FROM (
+SELECT * FROM S EXCEPT SELECT * FROM R) as table2
 ORDER BY person_id;
