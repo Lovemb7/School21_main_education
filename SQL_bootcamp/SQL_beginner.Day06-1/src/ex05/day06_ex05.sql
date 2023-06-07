10da2e82a0f224ed433cf8008519db14ec861c2f
finchren <finchren@student.21-school.ru>
2023-06-07 21:10:34 +0300

    Finished with the tasks for Tuesday

---
+++ SQL_bootcamp/SQL_beginner.Day06-1/src/ex05/day06_ex05.sql
@@ -0,0 +1,5 @@
+COMMENT ON TABLE person_discounts IS 'Personal discount table';
+COMMENT ON COLUMN person_discounts.id  IS 'ID of the person discount';
+COMMENT ON COLUMN person_discounts.person_id  IS 'ID of the person';
+COMMENT ON COLUMN person_discounts.pizzeria_id  IS 'ID of the pizzeria';
+COMMENT ON COLUMN person_discounts.discount  IS 'Personal discount value';
